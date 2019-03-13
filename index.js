c8b8e358caa7570d8c548b614faae0ad6def8243
Elise Phan <aliceconnic@gmail.com>
2019-03-13 19:29:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your solution in this file!
 var customerName = 'bob';
-const = 
+const = 'some initial value'
 
 function upperCaseCustomerName() {
  customerName = customerName.toUpperCase();
